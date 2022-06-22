3568bfda61164a05da127e429d5b3d78b6460606
Shota Mamulashvili <mamulashvili13@gmail.com>
2022-06-22 20:14:25 +0400

    Toggler

--- src/components/LeftSide/Header/index.tsx
+++ src/components/LeftSide/Header/index.tsx
@@ -1,12 +1,18 @@
 import './style.scss';
 import DateTime from './DateTime';
 import Welcome from './Welcome';
+import Toggler from '../../common/Toggler';
 
 const Header: React.FC = () => {
+
+
+
+
   return (
     <>
       <DateTime />
       <Welcome />
+      <Toggler handleChange={({ target: { checked } }) => console.log(checked)} />
     </>
   )
 }
--- src/components/common/Toggler/index.tsx
+++ src/components/common/Toggler/index.tsx
@@ -1,6 +1,25 @@
-const Toggler: React.FC = () => {
+import './style.scss';
+
+interface Props {
+  handleChange: (e: any) => void;
+}
+
+const Toggler: React.FC<Props> = ({ handleChange }) => {
   return (
-    <></>
+    <>
+      <div className="toggle-wrapper">
+        <input
+          type="checkbox"
+          className="dn"
+          id="dn"
+          onChange={e => handleChange(e)}
+        />
+        <label htmlFor="dn" className="toggle">
+          <span className="toggle-handler">
+          </span>
+        </label>
+      </div>
+    </>
   )
 }
 
